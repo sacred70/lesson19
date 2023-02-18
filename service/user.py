c8e8d03afe22ef5854d9deb0fe45fa46585a39ec
sacred70 <sacred70@mail.ru>
2023-02-18 12:57:22 +0700

    1

--- service/user.py
+++ service/user.py
@@ -23,4 +23,18 @@ class UserService:
         user_data['password'] = self.get_hash(user_data['password'])
         return self.dao.create(user_data)
 
+    def update(self, user_data):
+        user_data['password'] = self.get_hash(user_data['password'])
+        return self.dao.update(user_data)
 
+    def delete(self, uid):
+        return self.dao.delete(uid)
+
+    def get_hach(self, password):
+        """Принимает пароль, отдает хэш"""
+        return base64.b64encode(hashlib.pbkdf2_hmac(
+            'sha256',
+            password.encode('utf-8'),
+            PWD_HASH_SALT,
+            PWD_HASH_ITERATIONS
+        ))
