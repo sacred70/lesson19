66b5166ac60c9ec2e6163437ec9b4da039c65537
sacred70 <sacred70@mail.ru>
2023-02-18 13:03:12 +0700

    1

--- service/user.py
+++ service/user.py
@@ -8,13 +8,12 @@ from dao.user import UserDAO
 class UserService:
     def __init__(self, dao: UserDAO):
         self.dao = dao
+
     def get_one(self, uid):
         return self.dao.get_user_id(uid)
 
-
     def get_username(self, username):
         return self.dao.get_user_username(username)
-
 
     def get_all(self):
         return self.dao.get_all()
@@ -38,7 +37,6 @@ class UserService:
             PWD_HASH_SALT,
             PWD_HASH_ITERATIONS
         ))
-
 
     def compare_passwords(self,pass_hash, password):
         """Принимает хешированный пароль и введенный пароль, создает хеш введенного пароля,
