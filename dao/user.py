f7184f2897b0b61f79e83da75103f79a7a67dcf2
sacred70 <sacred70@mail.ru>
2023-02-18 12:17:39 +0700

    1

--- dao/user.py
+++ dao/user.py
@@ -4,3 +4,15 @@ from dao.model.user import User
 class UserDAO:
     def __init__(self, session):
         self.session = session
+
+
+    def get_all(self):
+        return self.session.query(User).all()
+
+
+    def get_user_id(self, uid):
+        return self.session.query(User).get(uid)
+
+
+
+
