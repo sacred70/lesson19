e7297cbc9114724ff5d71b3358c2da83351c8c90
sacred70 <sacred70@mail.ru>
2023-02-18 12:28:21 +0700

    1

--- dao/user.py
+++ dao/user.py
@@ -5,21 +5,26 @@ class UserDAO:
     def __init__(self, session):
         self.session = session
 
-
     def get_all(self):
         return self.session.query(User).all()
-
 
     def get_user_id(self, uid):
         return self.session.query(User).get(uid)
 
-
     def get_user_username(self, username):
         return self.session.query(User).filter(User.username == username).first()
 
-    def create(self, user_d):
-        user = User(**user_d)
+    def create(self, user_data):
+        user = User(**user_data)
         self.session.add(user)
         self.session.commit()
         return user
 
+    def update(self, user_data):
+        user = self.get_user_id(user_data.get("id"))
+        user.username = user_data.get("username")
+        user.password = user_data.get("password")
+        user.role = user_data.get("role")
+
+        self.session.add(user)
+        self.session.commit()
