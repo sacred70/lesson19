f16a0f9e47ebf7952f6ef8984558155c81c68de3
sacred70 <sacred70@mail.ru>
2023-02-18 12:52:25 +0700

    1

--- service/user.py
+++ service/user.py
@@ -8,4 +8,19 @@ from dao.user import UserDAO
 class UserService:
     def __init__(self, dao: UserDAO):
         self.dao = dao
+    def get_one(self, uid):
+        return self.dao.get_user_id(uid)
+
+
+    def get_username(self, username):
+        return self.dao.get_user_username(username)
+
+
+    def get_all(self):
+        return self.dao.get_all()
+
+    def create(self, user_data):
+        user_data['password'] = self.get_hash(user_data['password'])
+        return self.dao.create(user_data)
+
 
