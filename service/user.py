504682bba2f7712bdf8d3b9e1b78c909f9e29e10
sacred70 <sacred70@mail.ru>
2023-02-18 12:43:02 +0700

    1

--- service/user.py
+++ service/user.py
@@ -1,1 +1,11 @@
 from constants import PWD_HASH_SALT, PWD_HASH_ITERATIONS
+import base64
+import hashlib
+import hmac
+from dao.user import UserDAO
+
+
+class UserService:
+    def __init__(self, dao: UserDAO):
+        self.dao = dao
+
